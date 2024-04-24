e1861bcae1da09f304f8d8e161f5a51d6c245bb3
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-04-24 03:29:28 +0300

    PwdManager File Ops

---
+++ SayiTahminOyunu/sayiTahminOyunu.py
@@ -0,0 +1,9 @@
+# Bilgisayar 1-100 arasında bir sayı üretir
+# Kullanıcıya giriş yapması gereken aralık belirtilir
+# Kullanıcı sayıyı buluncaya kadar yönlendirilir
+# Örn.: Kullanıcı ilk iki tahmininde 15 ve 85 girdi ise:
+# Sayı girmesi gereken aralık "15-85 arasında bir sayı giriniz:" şeklinde olmalıdır
+# Kullanıcının yaptığı tahminler bir listede tutulur
+# Kullanıcının yaptığı tahmin sayısı ve yaptığı tahminler
+# oyun sonunda ekrana yazdırılır
+# Oyuncu oyundan çıkmak için q harfi ile giriş yapmalıdır
