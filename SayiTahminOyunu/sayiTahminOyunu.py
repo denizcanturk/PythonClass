199bcbc8738f11bf7f31cb1d060408bcad561152
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-04-25 21:27:59 +0300

    Sayi Tahmin Oyunumun ilk versiyonu

--- SayiTahminOyunu/sayiTahminOyunu.py
+++ SayiTahminOyunu/sayiTahminOyunu.py
@@ -1,4 +1,4 @@
-# Bilgisayar 1-100 arasında bir sayı üretir
+"""# Bilgisayar 1-100 arasında bir sayı üretir
 # Kullanıcıya giriş yapması gereken aralık belirtilir
 # Kullanıcı sayıyı buluncaya kadar yönlendirilir
 # - Örn.: Kullanıcı ilk iki tahmininde 15 ve 85 girdi ise:
@@ -6,4 +6,28 @@
 # Kullanıcının yaptığı tahminler bir listede tutulur
 # Kullanıcının yaptığı tahmin sayısı ve yaptığı tahminler
 # oyun sonunda ekrana yazdırılır
-# Oyuncu oyundan çıkmak için q harfi ile giriş yapmalıdır
+# Oyuncu oyundan çıkmak için q harfi ile giriş yapmalıdır"""
+from random import randint
+
+bilgisayarSecimi = randint(1,100)
+MIN = 1
+MAX = 100
+tahminListesi = []
+while True:
+    kullaniciSecimi = int(input("{}-{} arasında Sayi arassında giriniz : ".format(MIN,MAX)))
+    
+    tahminListesi.append(kullaniciSecimi)
+    
+    if kullaniciSecimi > bilgisayarSecimi:
+        print("Daha küçük olmalı")
+        MAX = kullaniciSecimi
+
+    if bilgisayarSecimi > kullaniciSecimi:
+        print("Daha büyük olmalı")
+        MIN = kullaniciSecimi
+
+    if bilgisayarSecimi == kullaniciSecimi:
+        print("Kutlaaarrııııızzzz....")
+        break
+
+print(tahminListesi, " tahmin sayısı: " , len(tahminListesi))
