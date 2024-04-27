b2cc8e3cc6b385cf07aff992cb1594043bef5d13
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-04-27 23:55:26 +0300

    Tekrar Dersi eklendi

---
+++ tekrarDersi/kutuphane.py
@@ -0,0 +1,9 @@
+
+PI = 3.14159265
+
+
+def topla(num1, num2):
+    return num1 + num2
+
+def carp(num1, num2):
+    return num1*num2
---
+++ tekrarDersi/tekrarDersi.py
@@ -0,0 +1,36 @@
+# x = 10
+
+# def karesi():
+#     global x
+#     x = 3
+#     print(x*x)
+
+# def degistir():
+#     global x
+#     x = 25
+
+
+# karesi()
+# degistir()
+# x = x/2
+# print(x)
+
+#------------------------------------------------------------
+
+# from kutuphane import PI as p
+# from kutuphane import topla, carp
+
+
+# print(topla(3,5))
+# print(carp(2,4))
+# print(p)
+
+#Sayının karekokunu al
+import random as r
+from random import randint
+import math as m
+from math import sqrt as s
+rastgele = randint(1,100)
+
+sonuc = s(100)
+print(sonuc)
