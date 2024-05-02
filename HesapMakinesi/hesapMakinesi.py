02bac102c92e0189d031ebe4fc1164cd618cab29
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-05-02 03:06:49 +0300

    Hata Yakalama Eklendi.

--- HesapMakinesi/hesapMakinesi.py
+++ HesapMakinesi/hesapMakinesi.py
@@ -38,44 +38,44 @@ def kuvvetiniAlma(num1, num2):
 
 def bolumdenKalaniniBulma(num1, num2):
     return num1 % num2
+while True : 
+    secim = input("1. Toplama\n" \
+                "2. Çıkarma\n" \
+                "3. Çarpma\n" \
+                "4. Bölme\n" \
+                "5. Kok Alma\n" \
+                "6. Kuvvetini Alma\n" \
+                "7. Bölümden Kalanı Bulma\n" \
+                "8. Çıkış\n" \
+                "Seçiminiz > " )
 
-secim = input("1. Toplama\n" \
-              "2. Çıkarma\n" \
-              "3. Çarpma\n" \
-              "4. Bölme\n" \
-              "5. Kok Alma\n" \
-              "6. Kuvvetini Alma\n" \
-              "7. Bölümden Kalanı Bulma\n" \
-              "8. Çıkış\n" \
-              "Seçiminiz > " )
+    if secim == "8":
+        exit()
 
-if secim == "8":
-    exit()
-
-if not secim.isdigit():
-    print("Lütfen 1-8 arası sayı giriniz.")
-elif int(secim) not in range(1,9):
-    print("Aralık dışı sayı girdiniz!!!")
+    if not secim.isdigit():
+        print("Lütfen 1-8 arası sayı giriniz.")
+    elif int(secim) not in range(1,9):
+        print("Aralık dışı sayı girdiniz!!!")
 
 
-a = int(input("1.Sayıyı Giriniz : "))
-b = int(input("2.Sayıyı Giriniz : "))
+    a = int(input("1.Sayıyı Giriniz : "))
+    b = int(input("2.Sayıyı Giriniz : "))
 
-sonuc = 0
+    sonuc = 0
 
-if secim == "1":
-   sonuc = toplama(a,b)
-elif secim == "2":
-    None
-elif secim == "3":
-    None
-elif secim == "4":
-    None
-elif secim == "5":
-    None
-elif secim == "6":
-    None
-elif secim == "7":
-    None
+    if secim == "1":
+        sonuc = toplama(a,b)
+    elif secim == "2":
+        None
+    elif secim == "3":
+        None
+    elif secim == "4":
+        None
+    elif secim == "5":
+        None
+    elif secim == "6":
+        None
+    elif secim == "7":
+        None
 
-print("İşleminizin sonucu : {}".format(sonuc))
+    print("İşleminizin sonucu : {}".format(sonuc))
