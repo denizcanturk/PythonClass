45a0af4633bb45446140dcd70ba1ef82bf7d664a
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-05-02 22:23:43 +0300

    tryExcept Module Eklendi.

--- HataYakalama/tryExcept.py
+++ HataYakalama/tryExcept.py
@@ -1,3 +1,18 @@
 # Bu dersin konusu Hata Yakalama...
 
-# Bizim programcı olarak yakalayamadığımız hataları, programın yakalaması için vaziyet alma...
+# Bizim programcı olarak yakalayamadığımız hataları, 
+# programın yakalaması için vaziyet alma...
+dosyaAdi = "metin.txt"
+try:
+    dosya = open(dosyaAdi,"r")
+    print(dosya.read())
+except IOError as i:
+    print("Dosya yok kardeşim")
+except NameError as r:
+    print("Degisken Tanımlanmadı...")
+except Exception as e:
+    print(str(e))
+else:
+     dosya.close()
+finally: 
+   print("Happened...")
