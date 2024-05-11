454cd9fccb75c72ea71e04177116d2c84e4ce123
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-05-11 23:07:47 +0300

    bişeyler bişeyler

--- Dosyaİslemleri/dosyaIslemleri.py
+++ Dosyaİslemleri/dosyaIslemleri.py
@@ -1,31 +1,14 @@
 #Absolute Path
 dosyaKonumu = "/home/debinci/Desktop/proje/Dosyaİslemleri/metin.txt"
 #Relative Path
-dk = "Dosyaİslemleri/metin.txt"
-dosya = open(dosyaKonumu, "r", encoding="utf-8")
+relativeDosyaKonumu = "Dosyaİslemleri/metin.txt"
 
-# print(dosya.tell())
-# icerik = dosya.read(10)
-
-# print(dosya.tell())
-# icerik2=dosya.read(3)
-
-# print(dosya.tell())
-# print(icerik)
-# print(icerik2)
-
-
-# satir1 = dosya.readline().replace("\n", "")
-# print(dosya.tell())
-# satir2 = dosya.readline()
-# print(dosya.tell())
-
-# print(satir1)
-# print(satir2, end="")
+dosya = open(dosyaKonumu, "r")
 
 satirlar = dosya.readlines()
 
-print(len(satirlar))
 
-# for satir in satirlar:
-#     print(satirlar)
+print(satirlar)
+
+for satir in satirlar:
+    print(satir)
