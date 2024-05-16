7541b0a59dc9933724fdf1c891f617f95730b980
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-05-16 21:28:03 +0300

    Sifre Yonetici Prtogram yazild.

--- Dosyaİslemleri/PwdManager/SifreYonetici.py
+++ Dosyaİslemleri/PwdManager/SifreYonetici.py
@@ -1,10 +1,84 @@
 # Kullanıcı adı ve şifresi bir text dosyasında tutulur
 #
 # Kullanıcı adı ve şifresi girilirken tek satırda, 
-#        boşluk bırakılmadan "|" karakteri ile ayrılarak girilmelidir
+#        boşluk bırakılmadan ":" karakteri ile ayrılarak girilmelidir
 #
 # Program, şifre dosyasını görüntüleme, kullanıcı ekleme, 
 #        mevcut kullanıcıya ait şifreyi değiştirebilme ve 
 #       kullanıcı silme özelliğine sahip olmalıdır
 #
 # Yeni kullanıcılar yeni bir satır olarak eklenir
+
+import os
+
+dosyaKonumu = "/home/debinci/Desktop/proje/Dosyaİslemleri/PwdManager/sifre.txt"
+
+
+
+def tumunuGoruntule(dosyaYolu):
+    if os.path.exists(dosyaYolu):
+
+        with open(dosyaYolu, "r") as dosya:
+            icerik = dosya.readlines()
+        
+            for satir in icerik:
+                kullaniciAdi, sifre = satir.split(":")
+                print("Kullanıcı Adi : {}, Sifre : {}".format(kullaniciAdi, sifre.replace("\n","")))
+    else:
+        print("Dosya mevcut değil...")
+        
+def kullaniciEkle(dosyaYolu):
+    if os.path.exists(dosyaYolu):
+        yeniKullaniciAdi = input("Kullanıcı Adı Giriniz :")
+        yenSifre = input("Sifre Giriniz :")
+
+        with open(dosyaYolu, "a") as dosya:
+            dosya.write(yeniKullaniciAdi+":"+yenSifre)
+    else:
+        print("Dosya mevcut değil...")
+        exit()
+
+def kullaniciSil(dosyaYolu):
+    if os.path.exists(dosyaYolu):
+        kullaniciAdi = input("Silinecek Kullanıcı Adini Girniz : ")
+        with open(dosyaYolu, "r") as dosya:
+            icerik = dosya.readlines()
+        
+        for i, satir in enumerate(icerik) :
+            if kullaniciAdi in satir:
+                icerik.pop(i)
+            else:
+                
+        with open(dosyaYolu,"w") as dosya:
+            dosya.writelines(icerik)
+
+    else:
+        print("Dosya yok...")
+
+
+
+print("Seçenekleriniz :\n"
+      "1. Kullanıcı ve Şifre Görüntüle\n"
+      "2. Kullanıcı Ekle\n"
+      "3. Kullanıcı Silme\n"
+      "4. şifre Değiştirme\n"
+      "5.Çıkış\n")
+
+cevap = input("Bir seçenek seçiniz :")
+
+if cevap==  "1":
+    tumunuGoruntule(dosyaKonumu)
+    #TumunuGoruntule
+elif cevap == "2":
+    kullaniciEkle(dosyaKonumu)
+    #Kullanıcı Ekle
+elif cevap =="3":
+    kullaniciSil(dosyaKonumu)
+    #Kullanıcı Silme
+elif cevap == "4":
+    None
+    #Sifre Değistirme
+elif cevap == "5":
+    exit()
+else:
+    print("Adamın canını sıkma... ")
--- Dosyaİslemleri/dosyaIslemleri.py
+++ Dosyaİslemleri/dosyaIslemleri.py
@@ -3,15 +3,14 @@ dosyaKonumu = "/home/debinci/Desktop/proje/Dosyaİslemleri/metin.txt"
 #Relative Path
 relativeDosyaKonumu = "Dosyaİslemleri/metin.txt"
 
-# dosya = open(dosyaKonumu, "w+", encoding="utf-8")
 
-# icerik = dosya.readlines()
-
-# icerik.insert(1,"Benim Adım Batman")
-
-# print(icerik)
-# dosya.seek(0)
-# dosya.writelines(icerik)
 import os
 
-with
+if os.path.exists(dosyaKonumu):
+    print("Doyayı buldum, işlem yapıyorum... ")
+    with open(dosyaKonumu, "r+") as dosya:
+        icerik = dosya.read()
+        print(icerik)
+else:
+    print("Dosya yok gözün kör olmasın... ")
+
