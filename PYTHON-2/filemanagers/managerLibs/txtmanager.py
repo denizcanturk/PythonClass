e972cb5f989bdac5795e151cd38c35554e02d74d
denizcanturk <canturk_d@hotmail.com>
2024-08-07 20:38:19 +0300

    FileManager implement edildi

---
+++ PYTHON-2/filemanagers/managerLibs/txtmanager.py
@@ -0,0 +1,8 @@
+from managerLibs.filemanager import FileManager
+
+class TxtManager(FileManager):
+    """
+        FileManager sınıfının Text Dosyaları için özelliştirilmiş implementasyonu
+    """
+
+    
